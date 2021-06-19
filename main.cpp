973c6ebb08ca0963de1a88f9b06bcb8d3eaf6e77
LnxyBTW <TTV_BTW73498@outlook.com>
2021-06-19 20:55:27 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -92,7 +92,7 @@ void keyboard(unsigned char key,int x,int y)
  
 /* Main function: GLUT runs as a console application starting at main()  */
 int main(int argc, char** argv) {
-   cout<<"Welcome To My App / Uygulamama Hoþgeldiniz / Dobrodošli U Moju Aplikaciju \n";
+   cout<<"Welcome To My App / Uygulamama Hoï¿½geldiniz / Dobrodoï¿½li U Moju Aplikaciju \n";
    glutInit(&argc, argv);                 // Initialize GLUT
    glutInitDisplayMode(GLUT_DOUBLE);
   glutInitWindowSize(1200,800); 
