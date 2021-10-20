4d25f51dbb614edf96ee8e4a3f518f6e84699e0d
LnxyBTW <TTV_BTW73498@outlook.com>
2021-10-20 11:39:54 +0300

    update

--- main.cpp
+++ main.cpp
@@ -261,7 +261,7 @@ void theCube()
     
     
     /////////////x      y    z
-    glTranslatef(-0.7, 0.5, 3.9);
+    glTranslatef(-0.8, 0.5, 3.9);
     glRotatef(270.0, 0.5, 0.0, 0.0);
     //glRotatef(270.0, 1.0, 0.0, 0.0);
     glutSolidSphere(0.150, 30, 50);
