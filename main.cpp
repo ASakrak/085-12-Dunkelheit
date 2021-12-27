b23ef0eaca1783b74a2a4d98ce60098642bead49
LnxyBTW <TTV_BTW73498@outlook.com>
2021-12-27 22:44:25 +0300

    Update(Changed Color)
    I changed color. It's looking very good. I liked.

--- main.cpp
+++ main.cpp
@@ -540,10 +540,10 @@ void display()
 void init()
 {
 	//this codes for color
-	GLfloat black[] = {0.0, 0.0, 0.0, 0.0};
-	GLfloat yellow[] = {1.0, 0.0, 0.0, 0.0};
-	GLfloat cyan[] = {1.0, 0.0, 0.0, 0.0};
-	GLfloat white[] = {1.0, 0.0, 0.0, 0.0};
+	GLfloat black[] = {0.0, 1.0, 0.0, 0.0};
+	GLfloat yellow[] = {1.0, 1.0, 0.0, 1.0};
+	GLfloat cyan[] = {1.0, 1.0, 1.0, 1.0};
+	GLfloat white[] = {1.0, 1.0, 1.0, 0.0};
 	GLfloat direction[] = {1.0, 1.0, 1.0, 1.0};
 
 	glMaterialfv(GL_FRONT, GL_AMBIENT_AND_DIFFUSE, cyan);
