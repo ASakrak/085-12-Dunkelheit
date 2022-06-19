d6c7d60ccc4fc2fc96b0be99ec1d75573553c77f
LnxyBTW <TTV_BTW73498@outlook.com>
2022-06-19 09:53:41 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,9 @@
-*//////////////////////
+*54asdzxczxc
+*/
 first of all I'm aware that the code is too complicated but I'm too busy to fix it.
 I'm using this app just to have fun with cpp and learn/try something.
 By the way so many code not running i know.
-*/
+*/*/
 
 //(It has no commercial purpose.)
 /////////////////////
